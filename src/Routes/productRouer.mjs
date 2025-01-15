628258877b59d708c35713bcab354b32a473f982
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2025-01-15 13:50:50 +0530

    saved

--- src/Routes/productRouer.mjs
+++ src/Routes/productRouer.mjs
@@ -15,10 +15,20 @@ productRouter.post("/add-product", async (req, res) => {
 
 productRouter.get("/get-all/by-id/:id", async (req, res) => {
   const Id = Number(req.params.id);
-  const resp = await db.product.findUnique({ where: { Id: Id } });
+  const resp = await db.product.findMany({ where: { UserId: Id } });
   res.status(200).json({ resp });
 });
 
+productRouter.put("/update/by-product-id/:pid", async (req, res) => {
+  const Id = Number(req.params.pid);
+  const resp = await db.product.update({ where: { Id: Id }, data: req.body });
+  res.status(200).json({ resp });
+});
 
+productRouter.delete("/delete/by-id/:id", async (req, res) => {
+  const Id = Number(req.params.id);
+  const resp = await db.product.delete({ where: { UserId: Id } });
+  res.status(200).json({ resp });
+});
 
 export default productRouter;
